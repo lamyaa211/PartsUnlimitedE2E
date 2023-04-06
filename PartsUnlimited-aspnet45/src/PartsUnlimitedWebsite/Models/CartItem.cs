9144436ff7ec6253f0263d1f5dfa0cdfb759bb6c
Lamyaa N <NAKRIMY_LAMYAA@emsi-edu.ma>
2023-04-06 09:45:52 +0000

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
-    {// My first change.
+    {// My second change.
         [Key]
         public int CartItemId { get; set; }
 
